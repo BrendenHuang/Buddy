cb3b8e75e33932698ebfbabcc06cd4b972c2df04
Brenden Huang <jxb2198@gmail.com>
2021-06-18 14:52:11 +0800

    update

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nyp.Buddy',
+  appName: 'Buddy',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
