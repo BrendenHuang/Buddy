7fe9e25ced829b06d1474b8c7813370f36386944
Yew Yen Sing <yewyensing@gmail.com>
2021-06-21 23:10:09 +0800

    Test

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,17 +1,13 @@
 import { Injectable } from '@angular/core';
-import { User } from '../models/users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  users: User[] = [];
   constructor() { }
 
-  getUsers(): User[]{
-    return this.users;
-  }
+ 
 }
 
 
